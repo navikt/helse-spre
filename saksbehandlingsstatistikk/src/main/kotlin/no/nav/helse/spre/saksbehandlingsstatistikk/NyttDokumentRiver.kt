505453433db96c77b4838b4461e908bb3f45efab
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2021-04-21 17:23:45 +0200

    Ignorer sykmelding-/inntektsmeldingId-er som ikke er gyldige UUID-er
    Dårlige data i dev..

--- saksbehandlingsstatistikk/src/main/kotlin/no/nav/helse/spre/saksbehandlingsstatistikk/NyttDokumentRiver.kt
+++ saksbehandlingsstatistikk/src/main/kotlin/no/nav/helse/spre/saksbehandlingsstatistikk/NyttDokumentRiver.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.spre.saksbehandlingsstatistikk
 
+import com.fasterxml.jackson.databind.JsonNode
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
@@ -14,10 +15,14 @@ internal class NyttDokumentRiver(rapidsConnection: RapidsConnection, private val
     init {
         River(rapidsConnection).apply {
             validate { it.requireKey("@id") }
-            validate { it.interestedIn("inntektsmeldingId", "id", "sykmeldingId") }
+            validate { it.interestedIn("sykmeldingId", ::erUuid) }
+            validate { it.interestedIn("inntektsmeldingId", ::erUuid) }
+            validate { it.interestedIn("id") }
             validate { it.requireAny("@event_name", listOf("ny_søknad", "inntektsmelding", "sendt_søknad_nav", "sendt_søknad_arbeidsgiver")) }
         }.register(this)
     }
+
+    private fun erUuid(jsonNode: JsonNode) = UUID.fromString(jsonNode.asText())
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
         val hendelseId = UUID.fromString(packet["@id"].textValue())
