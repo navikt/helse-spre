0b55a98d22b1210012b0c56ae22bb825ae91aee6
Erlend Viddal <eviddal@gmail.com>
2021-05-10 15:33:28 +0200

    Refaktorisering av tester
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>

--- saksbehandlingsstatistikk/src/test/kotlin/no/nav/helse/spre/saksbehandlingsstatistikk/EndToEndTest.kt
+++ saksbehandlingsstatistikk/src/test/kotlin/no/nav/helse/spre/saksbehandlingsstatistikk/EndToEndTest.kt
@@ -1,17 +1,12 @@
 package no.nav.helse.spre.saksbehandlingsstatistikk
 
-import com.fasterxml.jackson.module.kotlin.readValue
-import io.mockk.CapturingSlot
-import io.mockk.mockk
-import io.mockk.verify
 import kotliquery.queryOf
 import kotliquery.sessionOf
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import no.nav.helse.spre.saksbehandlingsstatistikk.Avsender.SPLEIS
 import no.nav.helse.spre.saksbehandlingsstatistikk.BehandlingStatus.AVSLUTTET
+import no.nav.helse.spre.saksbehandlingsstatistikk.TestUtil.finnSøknadDokumentId
 import no.nav.helse.spre.saksbehandlingsstatistikk.YtelseType.SYKEPENGER
-import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.ProducerRecord
 import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
@@ -23,7 +18,7 @@ internal class EndToEndTest {
     private val testRapid = TestRapid()
     private val dataSource = TestUtil.dataSource
     private val søknadDao = SøknadDao(dataSource)
-    private val utgiver = TestUtil.LokalUtgiver()
+    private val utgiver = LokalUtgiver()
     private val spreService = SpreService(utgiver, søknadDao)
 
     init {
@@ -42,7 +37,12 @@ internal class EndToEndTest {
 
     @Test
     fun `Spleis reagerer på søknad`() {
-        val søknad = Søknad(UUID.randomUUID(), UUID.randomUUID(), LocalDateTime.now(), LocalDateTime.now())
+        val søknad = Søknad(
+            UUID.randomUUID(),
+            UUID.randomUUID(),
+            LocalDateTime.now(),
+            LocalDateTime.now()
+        )
 
         testRapid.sendTestMessage(sendtSøknadNavMessage(søknad))
         testRapid.sendTestMessage(vedtakFattetMessage(listOf(søknad.hendelseId)))
@@ -73,6 +73,29 @@ internal class EndToEndTest {
     }
 
     @Test
+    fun `lagrer søknad til basen`() {
+        val søknadHendelseId = UUID.randomUUID()
+        val søknad = Søknad(søknadHendelseId, UUID.randomUUID(), LocalDateTime.now(), LocalDateTime.now(), null)
+
+        testRapid.sendTestMessage(sendtSøknadNavMessage(søknad))
+
+        val søknadDokumentId = finnSøknadDokumentId(søknadHendelseId)
+        assertEquals(søknad.dokumentId, søknadDokumentId)
+    }
+
+    @Test
+    fun `håndterer duplikate dokumenter`() {
+        val søknadHendelseId = UUID.randomUUID()
+        val søknad = Søknad(søknadHendelseId, UUID.randomUUID(), LocalDateTime.now(), LocalDateTime.now(), null)
+
+        testRapid.sendTestMessage(sendtSøknadArbeidsgiverMessage(søknad))
+        testRapid.sendTestMessage(sendtSøknadNavMessage(søknad))
+
+        val søknadDokumentId = finnSøknadDokumentId(søknadHendelseId)
+        assertEquals(søknad.dokumentId, søknadDokumentId)
+    }
+
+    @Test
     fun `Ignorerer vedtaksperiode_endret-events med dato fra før vi har en komplett dokumentdatabase`() {
         val søknad = Søknad(UUID.randomUUID(), UUID.randomUUID(), LocalDateTime.now(), LocalDateTime.now(), null)
 
@@ -80,7 +103,76 @@ internal class EndToEndTest {
 
         assert(utgiver.meldinger.isEmpty())
     }
+
+    @Test
+    fun `lagrer saksbehandlingsløp for søknad`() {
+
+        val vedtaksperiodeId = UUID.randomUUID()
+        val saksbehandlerIdent = "AA10000"
+
+        val nyttDokumentData = nyttDokumentData()
+
+        val søknad = nyttDokumentData.asSøknad
+            .saksbehandlerIdent(saksbehandlerIdent)
+            .vedtaksperiodeId(vedtaksperiodeId)
+
+
+
+
+        testRapid.sendTestMessage(nyttDokumentData.sendtSøknadNavMessage)
+        testRapid.sendTestMessage(VedtaksperiodeEndretData(listOf(nyttDokumentData.hendelseId), vedtaksperiodeId).json)
+        testRapid.sendTestMessage(vedtaksperiodeGodkjentMessage(saksbehandlerIdent, vedtaksperiodeId))
+        testRapid.sendTestMessage(vedtakFattetMessage(listOf(nyttDokumentData.hendelseId), vedtaksperiodeId))
+
+        assertEquals(søknad, søknadDao.finnSøknad(listOf(nyttDokumentData.hendelseId)))
+        assertEquals(søknad, søknadDao.finnSøknad(vedtaksperiodeId))
+    }
+
+
 }
+
+fun nyttDokumentData() = NyttDokumentData(
+    UUID.randomUUID(),
+    UUID.randomUUID(),
+    LocalDateTime.parse("2021-01-01T00:00:00"),
+    LocalDateTime.parse("2021-01-01T01:00:00"),
+)
+
+val NyttDokumentData.sendtSøknadNavMessage get() =
+    """{
+            "@event_name": "sendt_søknad_nav",
+            "@id": "${this.hendelseId}",
+            "id": "${this.søknadId}",
+            "sendtNav": "${this.mottattDato}",
+            "rapportertDato": "${this.registrertDato}"
+        }"""
+
+private fun vedtakFattetMessage(hendelseIder: List<UUID>, vedtaksperiodeId: UUID) =
+    """{
+            "@event_name": "vedtak_fattet",
+            "@opprettet": "2021-04-01T12:00:00.000000",
+            "aktørId": "aktørens id",
+            "hendelser": [${hendelseIder.joinToString { """"$it"""" }}],
+            "vedtaksperiodeId": "$vedtaksperiodeId"
+        }"""
+
+val VedtaksperiodeEndretData.json get() =
+    """{
+            "@event_name": "vedtaksperiode_endret",
+            "gjeldendeTilstand": "AVVENTER_GODKJENNING",
+            "hendelser": [${this.hendelser.joinToString { """"$it"""" }}],
+            "@opprettet": "2021-03-09T18:23:27.76939",
+            "aktørId": "aktørens id",
+            "vedtaksperiodeId": "${this.vedtaksperiodeId}"
+        }"""
+
+fun vedtaksperiodeGodkjentMessage(saksbehandlerIdent: String, vedtaksperiodeId: UUID) =
+    """{
+            "@event_name": "vedtaksperiode_godkjent",
+            "@opprettet": "2021-03-09T18:23:27.76939",
+            "saksbehandlerIdent": "${saksbehandlerIdent}",
+            "vedtaksperiodeId": "${vedtaksperiodeId}"
+        }"""
 
 fun vedtaksperiodeEndretMessageUtdatert(hendelser: List<UUID>, tilstand: String) =
     """{
@@ -105,6 +197,16 @@ fun vedtakFattetMessage(hendelser: List<UUID>) =
 fun sendtSøknadNavMessage(søknad: Søknad) =
     """{
             "@event_name": "sendt_søknad_nav",
+            "@id": "${søknad.hendelseId}",
+            "id": "${søknad.dokumentId}",
+            "sendtNav": "2021-01-01T00:00:00",
+            "rapportertDato": "2021-01-01T00:00:00"
+        }"""
+
+
+private fun sendtSøknadArbeidsgiverMessage(søknad: Søknad) =
+    """{
+            "@event_name": "sendt_søknad_arbeidsgiver",
             "@id": "${søknad.hendelseId}",
             "id": "${søknad.dokumentId}",
             "sendtNav": "2021-01-01T00:00:00",
--- saksbehandlingsstatistikk/src/test/kotlin/no/nav/helse/spre/saksbehandlingsstatistikk/TestUtil.kt
+++ saksbehandlingsstatistikk/src/test/kotlin/no/nav/helse/spre/saksbehandlingsstatistikk/TestUtil.kt
@@ -3,36 +3,50 @@ package no.nav.helse.spre.saksbehandlingsstatistikk
 import com.opentable.db.postgres.embedded.EmbeddedPostgres
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import kotliquery.queryOf
+import kotliquery.sessionOf
 import org.flywaydb.core.Flyway
+import org.intellij.lang.annotations.Language
+import java.util.*
 import javax.sql.DataSource
 
-class TestUtil {
-    companion object {
-        val dataSource: DataSource = dataSource()
-        private fun dataSource(): DataSource {
-            val embeddedPostgres = EmbeddedPostgres.builder().setPort(56789).start()
-            val hikariConfig = HikariConfig().apply {
-                this.jdbcUrl = embeddedPostgres.getJdbcUrl("postgres", "postgres")
-                maximumPoolSize = 3
-                minimumIdle = 1
-                idleTimeout = 10001
-                connectionTimeout = 1000
-                maxLifetime = 30001
-            }
-            return HikariDataSource(hikariConfig)
-                .apply {
-                    Flyway
-                        .configure()
-                        .dataSource(this)
-                        .load().also(Flyway::migrate)
-                }
+object TestUtil {
+    val dataSource: DataSource = dataSource()
+    private fun dataSource(): DataSource {
+        val embeddedPostgres = EmbeddedPostgres.builder().setPort(56789).start()
+        val hikariConfig = HikariConfig().apply {
+            this.jdbcUrl = embeddedPostgres.getJdbcUrl("postgres", "postgres")
+            maximumPoolSize = 3
+            minimumIdle = 1
+            idleTimeout = 10001
+            connectionTimeout = 1000
+            maxLifetime = 30001
         }
+        return HikariDataSource(hikariConfig)
+            .apply {
+                Flyway
+                    .configure()
+                    .dataSource(this)
+                    .load().also(Flyway::migrate)
+            }
     }
 
-    class LokalUtgiver : Utgiver {
-        val meldinger : MutableList<StatistikkEvent> = mutableListOf()
-        override fun publiserStatistikk(statistikkEvent: StatistikkEvent) {
-            meldinger.add(statistikkEvent)
-        }
+    fun finnSøknadDokumentId(søknadHendelseId: UUID) = sessionOf(dataSource).use { session ->
+        @Language("PostgreSQL")
+        val query = "SELECT * FROM søknad WHERE hendelse_id = ?::uuid"
+        session.run(
+            queryOf(query, søknadHendelseId)
+                .map { row -> UUID.fromString(row.string("dokument_id")) }.asSingle
+        )
     }
 }
+
+class LokalUtgiver : Utgiver {
+    val meldinger: MutableList<StatistikkEvent> = mutableListOf()
+    override fun publiserStatistikk(statistikkEvent: StatistikkEvent) {
+        meldinger.add(statistikkEvent)
+    }
+}
+
+
+
