27e5fa0aecc947159c6abd9c7cef716a6fe28215
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2021-06-18 10:08:22 +0200

    Fiks migreringen for ikke-cloud-bruk, les: tester
    Co-authored-by: Sindre Broch <sindre.broch@nav.no>

--- saksbehandlingsstatistikk/src/main/resources/db/migration/V8__lesetilgang_for_mennesker.sql
+++ saksbehandlingsstatistikk/src/main/resources/db/migration/V8__lesetilgang_for_mennesker.sql
@@ -1,1 +1,16 @@
+
+DO
+$do$
+    BEGIN
+        IF NOT EXISTS (
+            SELECT
+            FROM   pg_catalog.pg_roles
+            WHERE  rolname = 'my_user'
+        )
+        THEN
+            CREATE ROLE cloudsqliamuser;
+        END IF;
+    END
+$do$;
+
 alter default privileges in schema public grant all on tables to cloudsqliamuser;
