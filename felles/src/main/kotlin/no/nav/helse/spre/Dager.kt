66c17a0abe0f8d45b5a6ccbab83845a6678a4441
Knut Nygaard <knut.nygaard@nav.no>
2021-09-01 10:54:05 +0200

    Dager kan ikke være internal om vi skal bruke den i en annen modul

--- felles/src/main/kotlin/no/nav/helse/spre/Dager.kt
+++ felles/src/main/kotlin/no/nav/helse/spre/Dager.kt
@@ -4,12 +4,12 @@ import java.time.DayOfWeek
 import java.time.LocalDate
 import kotlin.streams.toList
 
-internal fun utbetalingsdager(fom: LocalDate, tom: LocalDate = fom) = dagerFraTil(fom, tom, Dagtype.UTBETALINGSDAG)
-internal fun arbeidsdager(fom: LocalDate, tom: LocalDate = fom) = dagerFraTil(fom, tom, Dagtype.ARBEIDSDAG)
-internal fun fridager(fom: LocalDate, tom: LocalDate = fom) = dagerFraTil(fom, tom, Dagtype.FRIDAG)
-internal fun ukjentDager(fom: LocalDate, tom: LocalDate = fom) = dagerFraTil(fom, tom, Dagtype.UKJENTDAG)
-internal fun foreldetDager(fom: LocalDate, tom: LocalDate = fom) = dagerFraTil(fom, tom, Dagtype.FORELDETDAG)
-internal fun avvistDager(fom: LocalDate, tom: LocalDate = fom) = dagerFraTil(fom, tom, Dagtype.AVVISTDAG)
+fun utbetalingsdager(fom: LocalDate, tom: LocalDate = fom) = dagerFraTil(fom, tom, Dagtype.UTBETALINGSDAG)
+fun arbeidsdager(fom: LocalDate, tom: LocalDate = fom) = dagerFraTil(fom, tom, Dagtype.ARBEIDSDAG)
+fun fridager(fom: LocalDate, tom: LocalDate = fom) = dagerFraTil(fom, tom, Dagtype.FRIDAG)
+fun ukjentDager(fom: LocalDate, tom: LocalDate = fom) = dagerFraTil(fom, tom, Dagtype.UKJENTDAG)
+fun foreldetDager(fom: LocalDate, tom: LocalDate = fom) = dagerFraTil(fom, tom, Dagtype.FORELDETDAG)
+fun avvistDager(fom: LocalDate, tom: LocalDate = fom) = dagerFraTil(fom, tom, Dagtype.AVVISTDAG)
 
 private fun dagerFraTil(fom: LocalDate, tom: LocalDate, type: Dagtype): List<Dag> {
     return fom.datesUntil(tom.plusDays(1)).map {
@@ -17,7 +17,7 @@ private fun dagerFraTil(fom: LocalDate, tom: LocalDate, type: Dagtype): List<Dag
     }.toList()
 }
 
-internal class Dag(private val dato: LocalDate, private val type: Dagtype) {
+class Dag(val dato: LocalDate, private val type: Dagtype) {
     override fun toString(): String {
         return """{
                    "dato": "$dato",
@@ -26,7 +26,7 @@ internal class Dag(private val dato: LocalDate, private val type: Dagtype) {
     }
 
     companion object {
-        internal fun List<Dag>.toJson(): String {
+        fun List<Dag>.toJson(): String {
             return """
                    [
                     ${this.joinToString()}
@@ -36,7 +36,7 @@ internal class Dag(private val dato: LocalDate, private val type: Dagtype) {
     }
 }
 
-internal enum class Dagtype(val vanligNavn: String, val helgenavn: String = vanligNavn) {
+enum class Dagtype(val vanligNavn: String, val helgenavn: String = vanligNavn) {
     UTBETALINGSDAG("NavDag", "NavHelgeDag"),
     ARBEIDSDAG("Arbeidsdag"),
     FRIDAG("Fridag"),
