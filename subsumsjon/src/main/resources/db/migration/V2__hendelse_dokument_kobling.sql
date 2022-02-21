ddf419d975e5a5fb471b6bb3a484a9ac804ab26b
Sondre Halvorsen <sondre.halvorsen@nav.no>
2022-02-21 19:45:48 +0100

    Fjern flytting av data fra migrering
    - Bruk original tabell til data er flyttet

--- subsumsjon/src/main/resources/db/migration/V2__hendelse_dokument_kobling.sql
+++ subsumsjon/src/main/resources/db/migration/V2__hendelse_dokument_kobling.sql
@@ -6,7 +6,3 @@ CREATE TABLE hendelse_dokument_kobling
     publisert TIMESTAMP,
     PRIMARY KEY (hendelse_id, dokument_id)
 );
-
-INSERT INTO hendelse_dokument_kobling (hendelse_id, dokument_id, hendelse_type, publisert)
-SELECT hendelse_id, dokument_id, hendelse_navn, publisert from hendelse_id_mapping
-ON CONFLICT DO NOTHING;
