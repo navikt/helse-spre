e75ad124ab42671408558434049aeeb743b54553
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-03-15 18:44:19 +0100

    Debug-logging for å finne ut om dette noensinne skjer
    Jeg tror denne slettekoden er tenkt å håndtere at en søknad endres fra
    "sendt til Nav" til "sendt til arbeidsgiver," det virker jo ikke som
    noe som kan skje?

--- saksbehandlingsstatistikk/src/main/kotlin/no/nav/helse/spre/saksbehandlingsstatistikk/SletteArbeidsgiversøknadRiver.kt
+++ saksbehandlingsstatistikk/src/main/kotlin/no/nav/helse/spre/saksbehandlingsstatistikk/SletteArbeidsgiversøknadRiver.kt
@@ -1,7 +1,10 @@
 package no.nav.helse.spre.saksbehandlingsstatistikk
 
 import io.prometheus.client.Counter
-import no.nav.helse.rapids_rivers.*
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.RapidsConnection
+import no.nav.helse.rapids_rivers.River
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -23,6 +26,8 @@ internal class SletteArbeidsgiversøknadRiver(
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
         val søknadData = SøknadData.fromJson(packet)
+        val antallSøknader = søknadDao.finnSøknader(søknadData.asSøknad.søknadHendelseId).size
+        log.info("Finner $antallSøknader rader(er) å slette for søknad med id ${søknadData.søknadId} og hendelseId ${søknadData.hendelseId}")
         søknadDao.slettSøknad(søknadData.asSøknad.søknadHendelseId)
         log.info("Søknad med id ${søknadData.søknadId} og hendelseId ${søknadData.hendelseId} slettet")
         counter.inc()
