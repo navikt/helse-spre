96863c734f65715631d959763bd07beef51dfeb9
Simen Ullern <simenullern@gmail.com>
2023-03-13 16:22:25 +0100

    Kan nå se bort ifra vedtaksperiode_endret -> TIL_INFOTRYGD
    Co-authored-by: David Steinsland <david@steinsland.email>

--- oppgaver/src/main/kotlin/no/nav/helse/spre/oppgaver/HåndterVedtaksperiodeendringer.kt
+++ oppgaver/src/main/kotlin/no/nav/helse/spre/oppgaver/HåndterVedtaksperiodeendringer.kt
@@ -30,9 +30,6 @@ class HåndterVedtaksperiodeendringer(
                 withMDC(mapOf("event" to "vedtaksperiode_endret", "tilstand" to gjeldendeTilstand, "forrigeTilstand" to forrigeTilstand)) {
                     when (gjeldendeTilstand) {
                         "AVVENTER_GODKJENNING", "AVVENTER_GODKJENNING_REVURDERING" -> oppgave.håndter(Hendelse.AvventerGodkjenning)
-                        // Når noe går til Infotrygd sender Spleis ut signal om "opprett_oppgave" eller "opprett_oppgave_for_speilsaksbehandlere"
-                        // derfor sendes det ikke noe signal om å opprette oppgave her
-                        "TIL_INFOTRYGD" -> oppgaveDAO.lagreVedtaksperiodeEndretTilInfotrygd(oppgave.hendelseId)
                         "AVSLUTTET" -> oppgave.håndter(Hendelse.Avsluttet)
                         "AVSLUTTET_UTEN_UTBETALING" -> oppgave.håndter(Hendelse.AvsluttetUtenUtbetaling)
                     }
