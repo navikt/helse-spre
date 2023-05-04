cdb177fa41c732eb1eb4ff1ad35506465f6be189
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2023-05-04 20:42:49 +0200

    Fjerner midlertidig kode for å unngå oppgaver på AUU'er som allerede er utbetalt i Infotrygd.

--- oppgaver/src/main/kotlin/no/nav/helse/spre/oppgaver/HåndterVedtaksperiodeForkastet.kt
+++ oppgaver/src/main/kotlin/no/nav/helse/spre/oppgaver/HåndterVedtaksperiodeForkastet.kt
@@ -1,7 +1,6 @@
 package no.nav.helse.spre.oppgaver
 
 import no.nav.helse.rapids_rivers.*
-import java.time.LocalDateTime
 import java.util.*
 
 class HåndterVedtaksperiodeForkastet(
@@ -16,20 +15,10 @@ class HåndterVedtaksperiodeForkastet(
         River(rapidsConnection).apply {
             validate { it.requireValue("@event_name", "vedtaksperiode_forkastet") }
             validate { it.requireKey("hendelser", "harPeriodeInnenfor16Dager", "forlengerPeriode", "fødselsnummer", "organisasjonsnummer") }
-            validate { it.interestedIn("tilstand", "@opprettet" )}
         }.register(this)
     }
 
-    private val sisteMelding = LocalDateTime.parse("2023-05-04T17:12:49.343741751")
-    private fun JsonMessage.auuUtbetaltIInfotrygd(): Boolean {
-        if (get("tilstand").asText() != "AVSLUTTET_UTEN_UTBETALING") return false
-        val auuUtbetaltIInfotrygd = get("@opprettet").asLocalDateTime() <= sisteMelding
-        if (auuUtbetaltIInfotrygd) sikkerLog.info("Dropper å lage oppgave. auuUtbetaltIInfotrygd:\n\t$${toJson()}")
-        return auuUtbetaltIInfotrygd
-    }
-
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
-        if (packet.auuUtbetaltIInfotrygd()) return
         val harPeriodeInnenfor16Dager = packet["harPeriodeInnenfor16Dager"].asBoolean()
         val forlengerPeriode = packet["forlengerPeriode"].asBoolean()
         val orgnummer = packet["organisasjonsnummer"].asText()
