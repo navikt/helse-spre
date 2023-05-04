a700083898e2b48472fb888cd163927cd09b445d
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2023-05-04 18:40:21 +0200

    Feil ID'er 😭

--- oppgaver/src/main/kotlin/no/nav/helse/spre/oppgaver/Oppgave.kt
+++ oppgaver/src/main/kotlin/no/nav/helse/spre/oppgaver/Oppgave.kt
@@ -199,12 +199,12 @@ class Oppgave(
             }
 
             override fun håndterLagOppgave(oppgave: Oppgave) {
-                sikkerLog.info("Dropper å lage oppgave for hendelseId=${oppgave.hendelseId}, dokumentId=${oppgave.hendelseId}, {}", keyValue("fødselsnummer", oppgave.fødselsnummer))
+                sikkerLog.info("Dropper å lage oppgave for hendelseId=${oppgave.hendelseId}, dokumentId=${oppgave.dokumentId}, {}", keyValue("fødselsnummer", oppgave.fødselsnummer))
                 //oppgave.tilstand(LagOppgave)
             }
 
             override fun håndterLagOppgavePåSpeilKø(oppgave: Oppgave) {
-                sikkerLog.info("Dropper å lage speiloppgave for hendelseId=${oppgave.hendelseId}, dokumentId=${oppgave.hendelseId}, {}", keyValue("fødselsnummer", oppgave.fødselsnummer))
+                sikkerLog.info("Dropper å lage speiloppgave for hendelseId=${oppgave.hendelseId}, dokumentId=${oppgave.dokumentId}, {}", keyValue("fødselsnummer", oppgave.fødselsnummer))
                 //oppgave.tilstand(LagOppgaveForSpeilsaksbehandlere)
             }
 
