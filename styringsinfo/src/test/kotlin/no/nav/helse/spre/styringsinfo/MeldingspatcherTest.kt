599f18e091ce170ab956468b0e1ed456c4dd43d3
Øyvind Hagen <oyvind.hagen@nav.no>
2023-09-27 12:45:36 +0200

    ✅ Refaktorert test
    Co-authored-by: Kjetil JD <kjetil.jorgensen-dahl@nav.no>
    Co-authored-by: Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>

--- styringsinfo/src/test/kotlin/no/nav/helse/spre/styringsinfo/MeldingspatcherTest.kt
+++ styringsinfo/src/test/kotlin/no/nav/helse/spre/styringsinfo/MeldingspatcherTest.kt
@@ -13,6 +13,35 @@ import java.util.*
 class MeldingspatcherTest {
 
     @Test
+    fun `hvis SendtSøknad ikke matcher patchLevel skal meldingen være uendret`() {
+        val meldingen = """
+            {
+              "@id": "08a92c25-0e59-452f-ba60-83b7515de8e5",
+              "sendtArbeidsgiver": "2023-06-01T10:00:00.0",
+              "sendtNav": null,
+              "korrigerer": "4c6f931d-63b6-3ff7-b3bc-74d1ad627201",
+              "fødselsnummer": "12345678910",
+              "fom": "2023-06-05",
+              "tom": "2023-06-11"
+            }
+            """
+        val input = SendtSøknad(
+            sendt = LocalDateTime.parse("2023-06-01T10:00:00.0"),
+            korrigerer = UUID.fromString("4c6f931d-63b6-3ff7-b3bc-74d1ad627201"),
+            fom = LocalDate.parse("2023-06-05"),
+            tom = LocalDate.parse("2023-06-11"),
+            hendelseId = UUID.fromString("08a92c25-0e59-452f-ba60-83b7515de8e5"),
+            melding = meldingen,
+            patchLevel = 42
+        )
+
+        val result = input
+            .patch(null, ::fjernFnrFraJsonString, 1)
+
+        assertEquals(input, result)
+    }
+
+    @Test
     fun `filtrerer vekk fnr i SendtSøknad`() {
         val input = SendtSøknad(
             sendt = LocalDateTime.parse("2023-06-01T10:00:00.0"),
@@ -35,11 +64,7 @@ class MeldingspatcherTest {
 
         val result = input.patch(null, ::fjernFnrFraJsonString, 1)
 
-        assertEquals(input.sendt, result.sendt)
-        assertEquals(input.fom, result.fom)
-        assertEquals(input.tom, result.tom)
-        assertEquals(input.hendelseId, result.hendelseId)
-        assertEquals(input.korrigerer, result.korrigerer)
+
         JSONAssert.assertEquals("""
             {
               "@id": "08a92c25-0e59-452f-ba60-83b7515de8e5",
@@ -54,7 +79,6 @@ class MeldingspatcherTest {
         )
         assertEquals(1, result.patchLevel)
     }
-
 
     @Test
     fun `filtrerer vekk fødselsnummer i SendtSøknad`() {
@@ -90,45 +114,6 @@ class MeldingspatcherTest {
             }
         """, result.melding, STRICT_ORDER)
         assertEquals(1, result.patchLevel)
-    }
-
-    @Test
-    fun `hvis SendtSøknad ikke har null som patchLevel skal vi ikke fjerne fnr`() {
-        val input = SendtSøknad(
-            sendt = LocalDateTime.parse("2023-06-01T10:00:00.0"),
-            korrigerer = UUID.fromString("4c6f931d-63b6-3ff7-b3bc-74d1ad627201"),
-            fom = LocalDate.parse("2023-06-05"),
-            tom = LocalDate.parse("2023-06-11"),
-            hendelseId = UUID.fromString("08a92c25-0e59-452f-ba60-83b7515de8e5"),
-            melding = """
-            {
-              "@id": "08a92c25-0e59-452f-ba60-83b7515de8e5",
-              "sendtArbeidsgiver": "2023-06-01T10:00:00.0",
-              "sendtNav": null,
-              "korrigerer": "4c6f931d-63b6-3ff7-b3bc-74d1ad627201",
-              "fødselsnummer": "12345678910",
-              "fom": "2023-06-05",
-              "tom": "2023-06-11"
-            }
-            """,
-            patchLevel = 42
-        )
-
-        val result = input
-            .patch(null, ::fjernFnrFraJsonString, 1)
-
-        JSONAssert.assertEquals("""
-            {
-              "@id": "08a92c25-0e59-452f-ba60-83b7515de8e5",
-              "sendtArbeidsgiver": "2023-06-01T10:00:00.0",
-              "sendtNav": null,
-              "korrigerer": "4c6f931d-63b6-3ff7-b3bc-74d1ad627201",
-              "fødselsnummer": "12345678910",
-              "fom": "2023-06-05",
-              "tom": "2023-06-11"
-            }
-        """, result.melding, STRICT_ORDER)
-        assertEquals(42, result.patchLevel)
     }
 }
 
