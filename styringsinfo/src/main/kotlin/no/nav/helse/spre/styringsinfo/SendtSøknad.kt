5ad56e2d094b09c0d22c1f3a2901eeb9a7a19955
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2023-09-28 12:41:34 +0200

    ✨ Legg til funksjon for å fjerne fnr fra JSON
    Koden er ennå ikke tatt i bruk
    
    Co-authored-by: Øyvind Hagen <oyvind.hagen@nav.no>

--- styringsinfo/src/main/kotlin/no/nav/helse/spre/styringsinfo/SendtSøknad.kt
+++ styringsinfo/src/main/kotlin/no/nav/helse/spre/styringsinfo/SendtSøknad.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.spre.styringsinfo
 
+import com.fasterxml.jackson.databind.node.ObjectNode
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.UUID
@@ -13,3 +14,23 @@ data class SendtSøknad(
     val melding: String,
     val patchLevel: Int? = null
 )
+
+private val verdierSomSkalBort = listOf("fnr")
+
+fun fjernFnrFraJsonString(soknad: SendtSøknad): SendtSøknad {
+    val objectNode = objectMapper.readTree(soknad.melding) as ObjectNode
+    verdierSomSkalBort.map { objectNode.remove(it) }
+    val jsonUtenFnr = objectMapper.writeValueAsString(objectNode)
+    return soknad.copy(melding = jsonUtenFnr)
+}
+
+fun SendtSøknad.patch(
+    patchLevelPreCondition: Int?,
+    patchFunction: (input: SendtSøknad) -> SendtSøknad,
+    patchLevelPostPatch: Int
+): SendtSøknad {
+    if (this.patchLevel != patchLevelPreCondition) {
+        return this
+    }
+    return patchFunction(this).copy(patchLevel = patchLevelPostPatch)
+}
---
+++ styringsinfo/src/test/kotlin/no/nav/helse/spre/styringsinfo/SendtSøknadTest.kt
@@ -0,0 +1,64 @@
+package no.nav.helse.spre.styringsinfo
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.skyscreamer.jsonassert.JSONAssert
+import org.skyscreamer.jsonassert.JSONCompareMode.*
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
+
+class SendtSøknadTest {
+
+    @Test
+    fun `hvis SendtSøknad ikke matcher patchLevel skal meldingen være uendret`() {
+        val input = enSendtSøknad(enMelding()).copy(patchLevel = 42)
+        val result = input.patch(null, ::fjernFnrFraJsonString, 1)
+        assertEquals(input, result)
+    }
+
+    @Test
+    fun `filtrerer vekk fnr i SendtSøknad på upatchede meldinger - og setter patchLevel`() {
+        val input = enSendtSøknad(enMelding())
+
+        val result = input.patch(null, ::fjernFnrFraJsonString, 1)
+
+        JSONAssert.assertEquals(enMeldingUtenFnr(), result.melding, STRICT_ORDER)
+        assertEquals(1, result.patchLevel)
+    }
+
+
+    private fun enMelding() = """
+            {
+              "@id": "08a92c25-0e59-452f-ba60-83b7515de8e5",
+              "sendtArbeidsgiver": "2023-06-01T10:00:00.0",
+              "sendtNav": null,
+              "korrigerer": "4c6f931d-63b6-3ff7-b3bc-74d1ad627201",
+              "fødselsnummer": "12345678910",
+              "fom": "2023-06-05",
+              "tom": "2023-06-11"
+            }
+            """
+
+    private fun enSendtSøknad(meldingen: String) =
+        SendtSøknad(
+            sendt = LocalDateTime.parse("2023-06-01T10:00:00.0"),
+            korrigerer = UUID.fromString("4c6f931d-63b6-3ff7-b3bc-74d1ad627201"),
+            fom = LocalDate.parse("2023-06-05"),
+            tom = LocalDate.parse("2023-06-11"),
+            hendelseId = UUID.fromString("08a92c25-0e59-452f-ba60-83b7515de8e5"),
+            melding = meldingen,
+            patchLevel = null
+        )
+
+    private fun enMeldingUtenFnr() = """
+                {
+                  "@id": "08a92c25-0e59-452f-ba60-83b7515de8e5",
+                  "sendtArbeidsgiver": "2023-06-01T10:00:00.0",
+                  "sendtNav": null,
+                  "korrigerer": "4c6f931d-63b6-3ff7-b3bc-74d1ad627201",
+                  "fom": "2023-06-05",
+                  "tom": "2023-06-11"
+                }
+            """
+}
