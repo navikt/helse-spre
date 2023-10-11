1a94799bea14138651969c2700e0e8e7ea3fb34d
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2023-10-11 09:32:05 +0200

    🔊 Forbedret logging

--- styringsinfo/src/main/kotlin/no/nav/helse/spre/styringsinfo/SendtSøknadPatcher.kt
+++ styringsinfo/src/main/kotlin/no/nav/helse/spre/styringsinfo/SendtSøknadPatcher.kt
@@ -11,19 +11,25 @@ class SendtSøknadPatcher(
         antallMeldinger: Int
     ) {
         Thread.sleep(initialSleepMillis)
+        var antallPatchet = 0
         do {
             Thread.sleep(loopSleepMillis)
-            val søknader = sendtSøknadDao.hentMeldingerMedPatchLevelMindreEnn(patchLevelMindreEnn, antallMeldinger = antallMeldinger)
+            val søknader = sendtSøknadDao.hentMeldingerMedPatchLevelMindreEnn(
+                patchLevel = patchLevelMindreEnn,
+                antallMeldinger = antallMeldinger
+            )
 
             if (søknader.isEmpty()) {
-                log.info("Avslutter patching siden ingen søknader har patchLevel mindre enn $patchLevelMindreEnn.")
+                log.info("Avslutter patching siden ingen flere søknader har patchLevel mindre enn $patchLevelMindreEnn.")
                 return
             }
-            log.info("Hentet ${søknader.size} soknader med patchLevel mindre enn $patchLevelMindreEnn.")
+            log.info("Hentet ${søknader.size} søknader med patchLevel mindre enn $patchLevelMindreEnn.")
 
-            søknader.forEach {
-                it.patch().also { patchetSoknad -> sendtSøknadDao.oppdaterMelding(patchetSoknad) }
+            søknader.sumOf { sendtSøknadDao.oppdaterMelding(it.patch()) }.also {
+                antallPatchet += it
+                log.info("Patchet $it søknader. Totalt patchet: $antallPatchet")
             }
+
         } while (søknader.isNotEmpty())
     }
 }
