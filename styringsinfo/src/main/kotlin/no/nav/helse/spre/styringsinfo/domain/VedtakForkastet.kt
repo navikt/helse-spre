9ff16d5c849df497128c2afd4657016d01cb5309
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-10-12 14:04:04 +0200

    ✨ Legg til patch-metoder på VedtakForkastet
    Co-authored-by: Øyvind Hagen <oyvind.hagen@nav.no>

--- styringsinfo/src/main/kotlin/no/nav/helse/spre/styringsinfo/domain/VedtakForkastet.kt
+++ styringsinfo/src/main/kotlin/no/nav/helse/spre/styringsinfo/domain/VedtakForkastet.kt
@@ -1,8 +1,9 @@
 package no.nav.helse.spre.styringsinfo.domain
 
+import no.nav.helse.spre.styringsinfo.fjernNoderFraJson
 import java.time.LocalDate
 import java.time.LocalDateTime
-import java.util.UUID
+import java.util.*
 
 data class VedtakForkastet(
     val fom: LocalDate,
@@ -11,5 +12,20 @@ data class VedtakForkastet(
     val hendelseId: UUID,
     val melding: String,
     val hendelser: List<UUID>,
-    val patchLevel: Int? = null
-)
+    val patchLevel: Int = 0
+) {
+    fun patch() = this.patch(0, ::fjernFødselsnummerFraJsonString, 1)
+}
+
+private fun fjernFødselsnummerFraJsonString(vedtakForkastet: VedtakForkastet) =
+    vedtakForkastet.copy(melding = fjernNoderFraJson(vedtakForkastet.melding, listOf("fødselsnummer")))
+
+private fun VedtakForkastet.patch(
+    patchLevelPreCondition: Int,
+    patchFunction: (input: VedtakForkastet) -> VedtakForkastet,
+    patchLevelPostPatch: Int
+): VedtakForkastet =
+    if (this.patchLevel == patchLevelPreCondition)
+        patchFunction(this).copy(patchLevel = patchLevelPostPatch)
+    else
+        this
