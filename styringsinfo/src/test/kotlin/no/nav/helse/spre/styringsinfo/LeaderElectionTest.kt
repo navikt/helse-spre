b836a657c7b012097dcff98e0c5bb438134e5212
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-11-05 10:41:50 +0100

    ✅ Test leaderElection retry og timeout
    Lagt til retry test for å dokumentere bug i Ktor.

--- styringsinfo/src/test/kotlin/no/nav/helse/spre/styringsinfo/LeaderElectionTest.kt
+++ styringsinfo/src/test/kotlin/no/nav/helse/spre/styringsinfo/LeaderElectionTest.kt
@@ -7,40 +7,21 @@ import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.http.*
 import io.ktor.serialization.jackson.*
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class LeaderElectionTest {
 
-    private val mockHttpClient = createMockHttpClient()
-
-    private fun createMockHttpClient(): HttpClient {
-        return HttpClient(MockEngine) {
-            install(ContentNegotiation) {
-                jackson {
-                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                }
-            }
-            install(HttpTimeout) {
-                requestTimeoutMillis = 500
-            }
-            engine {
-                addHandler {
-                    respond(
-                        content = "{\"name\":\"leader-podname\",\"last_update\":\"2023-10-10T10:00:00Z\"}".toByteArray(),
-                        headers = headersOf("Content-Type", ContentType.Application.Json.toString())
-                    )
-                }
-            }
-        }
-    }
 
     @Test
     fun `Pod er leder`() {
         val leaderElection = LeaderElection(
-            httpClient = mockHttpClient,
+            httpClient = lagMockHttpClient(),
             hostname = "leader-podname",
         )
 
@@ -52,7 +33,7 @@ class LeaderElectionTest {
     @Test
     fun `Pod er ikke leder`() {
         val leaderElection = LeaderElection(
-            httpClient = mockHttpClient,
+            httpClient = lagMockHttpClient(),
             hostname = "other-podname",
         )
 
@@ -60,4 +41,96 @@ class LeaderElectionTest {
             assertFalse(leaderElection.isLeader())
         }
     }
+
+    @Test
+    fun `Det kastes exception når kall til elector timer ut`() {
+        val httpClient = HttpClient(MockEngine) {
+            install(HttpTimeout) {
+                requestTimeoutMillis = 100
+            }
+            engine {
+                addHandler {
+                    delay(200)
+                    lagLeaderElectionResponse()
+                }
+            }
+        }
+
+        val leaderElection = LeaderElection(
+            httpClient = httpClient,
+            hostname = "not-leader",
+        )
+
+        assertThrows(HttpRequestTimeoutException::class.java) {
+            runBlocking {
+                leaderElection.isLeader()
+            }
+        }
+    }
+
+    @Test
+    fun `Retires må gjøres innenfor timeout scope`() {
+        val maxAntallRetries = 2
+        var antallRetries = 0
+
+        val httpClient = HttpClient(MockEngine) {
+            install(ContentNegotiation) {
+                jackson {
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                }
+            }
+            install(HttpTimeout) {
+                // Hvis den ikke settes høyt nok, vil det kastes HttpRequestTimeoutException før alle retries er gjort.
+                // https://youtrack.jetbrains.com/issue/KTOR-4652/Retry-and-timeout-client-plugins-dont-work-together
+                // https://youtrack.jetbrains.com/issue/KTOR-5466/Connect-timeout-is-not-respected-when-using-the-HttpRequestRetry-plugin
+                requestTimeoutMillis = 1500
+            }
+            install(HttpRequestRetry) {
+                // Burde, i motsettning til retryOnServerErrors(), fange opp HttpRequestTimeoutException.
+                retryOnExceptionOrServerErrors(maxRetries = maxAntallRetries)
+                constantDelay(50)
+            }
+            engine {
+                addHandler {
+                    if (antallRetries < maxAntallRetries) {
+                        antallRetries++
+                        respondError(HttpStatusCode.ServiceUnavailable)
+                    } else {
+                        lagLeaderElectionResponse()
+                    }
+                }
+            }
+        }
+
+        val leaderElection = LeaderElection(
+            httpClient = httpClient,
+            hostname = "leader-podname",
+        )
+
+        runBlocking {
+            val isLeader = leaderElection.isLeader()
+            assertEquals(maxAntallRetries, antallRetries)
+            assertEquals(true, isLeader)
+        }
+    }
+
+    private fun lagMockHttpClient(): HttpClient {
+        return HttpClient(MockEngine) {
+            install(ContentNegotiation) {
+                jackson {
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                }
+            }
+            engine {
+                addHandler {
+                    lagLeaderElectionResponse()
+                }
+            }
+        }
+    }
+
+    private fun MockRequestHandleScope.lagLeaderElectionResponse() = respond(
+        content = "{\"name\":\"leader-podname\",\"last_update\":\"2023-10-10T10:00:00Z\"}".toByteArray(),
+        headers = headersOf("Content-Type", ContentType.Application.Json.toString()),
+    )
 }
