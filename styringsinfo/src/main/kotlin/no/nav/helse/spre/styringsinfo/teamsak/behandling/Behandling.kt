077fe205cd4f84f055192e432d0e824d173d7146
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-01-29 16:19:03 +0100

    Legger til en versjon på behandlingen

--- styringsinfo/src/main/kotlin/no/nav/helse/spre/styringsinfo/teamsak/behandling/Behandling.kt
+++ styringsinfo/src/main/kotlin/no/nav/helse/spre/styringsinfo/teamsak/behandling/Behandling.kt
@@ -1,9 +1,18 @@
 package no.nav.helse.spre.styringsinfo.teamsak.behandling
 
 import java.time.LocalDateTime
+import java.time.LocalDateTime.MIN
 import java.util.UUID
 internal data class SakId(val id: UUID)
 internal data class BehandlingId(val id: UUID)
+internal class Versjon private constructor(val versjon: String) {
+    init { check(versjon.matches("\\d\\.\\d\\.\\d".toRegex())) { "Ugyldig versjon $versjon" } }
+    override fun equals(other: Any?) = other is Versjon && this.versjon == other.versjon
+    override fun hashCode() = versjon.hashCode()
+    internal companion object {
+        internal fun of(versjon: String) = Versjon(versjon)
+    }
+}
 
 internal data class Behandling(
     internal val sakId: SakId,
@@ -14,7 +23,8 @@ internal data class Behandling(
     internal val registrertTid: LocalDateTime,       // Tidspunkt da behandlingen første gang ble registrert i fagsystemet. Ved digitale søknader bør denne være tilnærmet lik mottattTid.
     internal val funksjonellTid: LocalDateTime,      // Tidspunkt for siste endring på behandlingen. Ved første melding vil denne være lik registrertTid.
     internal val tekniskTid: LocalDateTime,          // Tidspunktet da fagsystemet legger hendelsen på grensesnittet/topicen.
-    internal val behandlingStatus: BehandlingStatus
+    internal val behandlingStatus: BehandlingStatus,
+    internal val versjon: Versjon = NåværendeVersjon
 ){
     internal enum class BehandlingStatus {
         KomplettFraBruker,
@@ -23,7 +33,14 @@ internal data class Behandling(
         BehandlesIInfotrygd
     }
 
-    private val utenMagiskeTimestamps get() = copy(funksjonellTid = LocalDateTime.MIN, tekniskTid = LocalDateTime.MIN)
+    private fun funksjoneltLik(other: Behandling): Boolean {
+        return copy(funksjonellTid = MIN, tekniskTid = MIN, versjon = Versjonløs) == other.copy(funksjonellTid = MIN, tekniskTid = MIN, versjon = Versjonløs)
+    }
+
+    private companion object {
+        val Versjonløs = Versjon.of("0.0.0")
+        val NåværendeVersjon = Versjon.of("0.0.1")
+    }
 
     class Builder(private val forrige: Behandling) {
         private lateinit var funksjonellTid: LocalDateTime // Denne _må_ alltid settes
@@ -45,7 +62,7 @@ internal data class Behandling(
                 tekniskTid = LocalDateTime.now(),
                 behandlingStatus = behandlingStatus ?: forrige.behandlingStatus
             )
-            if (forrige.utenMagiskeTimestamps == ny.utenMagiskeTimestamps) return null // Ikke noe ny info
+            if (ny.funksjoneltLik(forrige)) return null // Ikke noe ny info
             return ny
         }
     }
