3c2d66225a733cb7ded6baa3cf82fe7e093c47f8
Simen Ullern <simenullern@gmail.com>
2024-02-01 15:25:37 +0100

    Utdyper hvorfor blob-design
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>
    Co-authored-by: Erik Maximilian Forsman <erik.maximilian.forsman@nav.no>

--- styringsinfo/src/test/resources/db/migration/V27__behandling.sql
+++ styringsinfo/src/test/resources/db/migration/V27__behandling.sql
@@ -9,3 +9,7 @@ create table behandling(
 
 comment on column behandling.versjon is 'Versjon av data-kolonnen.';
 comment on column behandling.data is 'En blob for aa unngaa problemer med datastream.';
+-- Problem med datastream: Enkelte endringer på skjemaet støttes ikke i datastream
+-- Dette gjelder eksempelvis endre datatype på en kolonne eller fjerne en kolonne
+-- Se begrenser i "What are the limitations on the data that Datastream can process?" her https://cloud.google.com/datastream/docs/faq
+-- Når vi gjør endringer i felter gjør vi derfor endringer i data-blobben samt øker versjon-feltet
