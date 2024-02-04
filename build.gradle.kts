cff2d9f46d3cb5cb9cbc92abd51dfa2803491bb2
david steinsland <david@davidsteinsland.net>
2024-02-04 22:17:25 +0100

    bumper r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 val gradlewVersion = "8.5"
 val junitJupiterVersion = "5.10.1"
-val rapidsAndRiversVersion = "2024020421571707080262.b74e641fd6dd"
+val rapidsAndRiversVersion = "2024020422121707081126.d834a5d05bb2"
 val ktorVersion = "2.3.7" // should be set to same value as rapids and rivers
 val mockkVersion = "1.13.9"
 val testcontainersVersion = "1.19.3"
