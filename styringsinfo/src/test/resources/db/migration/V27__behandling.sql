45649b55d7f80791a8d12d8ad0089074f1eb2e41
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-02-12 10:27:40 +0100

    Kaster inn noen indexer 🤾

--- styringsinfo/src/test/resources/db/migration/V27__behandling.sql
+++ styringsinfo/src/test/resources/db/migration/V27__behandling.sql
@@ -2,8 +2,8 @@ create table behandling(
     sekvensnummer   SERIAL PRIMARY KEY,
     sakId           UUID NOT NULL,
     behandlingId    UUID NOT NULL,
-    funksjonellTid  TIMESTAMP NOT NULL,
-    tekniskTid      TIMESTAMP NOT NULL DEFAULT (now() AT TIME ZONE 'Europe/Oslo'),
+    funksjonellTid  TIMESTAMP(6) NOT NULL,
+    tekniskTid      TIMESTAMP(6) NOT NULL DEFAULT (now() AT TIME ZONE 'Europe/Oslo'),
     versjon         VARCHAR NOT NULL,
     siste           BOOLEAN NOT NULL,
     data            JSONB NOT NULL
@@ -11,8 +11,11 @@ create table behandling(
 
 comment on column behandling.versjon is 'Versjon av data-kolonnen.';
 comment on column behandling.data is 'En blob for aa unngaa problemer med datastream.';
-comment on column behandling.tekniskTid is 'Tidspunktet da fagsystemet legger hendelsen på grensesnittet/topicen.'
+comment on column behandling.tekniskTid is 'Tidspunktet da fagsystemet legger hendelsen på grensesnittet/topicen.';
 -- Problem med datastream: Enkelte endringer på skjemaet støttes ikke i datastream
 -- Dette gjelder eksempelvis endre datatype på en kolonne eller fjerne en kolonne
 -- Se begrenser i "What are the limitations on the data that Datastream can process?" her https://cloud.google.com/datastream/docs/faq
 -- Når vi gjør endringer i felter gjør vi derfor endringer i data-blobben samt øker versjon-feltet
+
+create index idx_sisteBehandlingerPerSak on behandling(sakId, siste);
+create index idx_sisteBehandling on behandling(behandlingId, siste);
