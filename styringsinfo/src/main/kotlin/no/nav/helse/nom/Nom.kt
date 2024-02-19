4d10416fa6bd18feff625061960b8bac17188e3d
Simen Ullern <simenullern@gmail.com>
2024-02-19 14:49:20 +0100

    Kan parse enhet fra nom
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
    Co-authored-by: Øyvind Hagen <oyvind.hagen@nav.no>
    Co-authored-by: Christina Grønhaug <christina.gronhaug@nav.no>

--- styringsinfo/src/main/kotlin/no/nav/helse/nom/Nom.kt
+++ styringsinfo/src/main/kotlin/no/nav/helse/nom/Nom.kt
@@ -1,7 +1,59 @@
 package no.nav.helse.nom
 
+import com.fasterxml.jackson.databind.JsonNode
+import no.nav.helse.rapids_rivers.asLocalDate
+import no.nav.helse.rapids_rivers.asOptionalLocalDate
+import java.time.LocalDate
+
 typealias Saksbehandler = String
 typealias Enhet = String
+
+private const val dollar = '$'
 class Nom {
-    fun enhent(ident: Saksbehandler): Enhet = "Dette er ikke en ekte enhet"
+    fun enhet(ident: Saksbehandler, gyldigPåDato: LocalDate): Enhet = "Dette er ikke en ekte enhet"
+
+        private val finnEnhetQuery: String = """
+        query enhet(`$dollar`navIdent: String!) {
+          ressurs(where: {navident: `$dollar`navIdent}) {
+              navident
+              orgTilknytning {
+                  gyldigFom
+                  gyldigTom
+                  orgEnhet {
+                      id
+                      navn
+                      remedyEnhetId
+                      orgEnhetsType
+                  }
+              }
+          }
+        }
+""".trimIndent()
+
 }
+internal fun JsonNode.enhet(gyldigPåDato: LocalDate): String? {
+    val tilknytninger = this["data"]["ressurs"]["orgTilknytning"].map {
+        Tilknytning(
+            gyldigFom = it["gyldigFom"].asLocalDate(),
+            gyldigTom = it["gyldigTom"].asOptionalLocalDate(),
+            orgEnhetsType = it["orgEnhet"]["orgEnhetsType"].asText(),
+            enhet = it["orgEnhet"]["remedyEnhetId"].asText()
+        )
+    }
+    val kandidater = tilknytninger.filter { it.gyldigFom < gyldigPåDato && (it.gyldigTom == null || it.gyldigTom > gyldigPåDato)}
+    val sortert = kandidater.sortedWith { a, _ ->
+        when {
+            a.orgEnhetsType == "NAV_ARBEID_OG_YTELSER" -> -1
+            else -> 0
+        }
+    }
+    return sortert.firstOrNull()?.enhet
+}
+
+data class Tilknytning(
+    val gyldigFom: LocalDate,
+    val gyldigTom: LocalDate?,
+    val orgEnhetsType: String,
+    val enhet: String
+)
+
