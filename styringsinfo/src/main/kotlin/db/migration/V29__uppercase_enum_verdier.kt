416a23fea6007f43d9a5e1b27daa6aafbf366e3e
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-02-29 13:36:30 +0100

    Skriver om V29 til BehandlingshendelseJsonMigrering
    BehandlingshendelseJsonMigrering ble skrevet med sterk inspirasjon fra V29, nå får den selv lov til å være en BehandlingshendelseJsonMigrering.
    
    Ettersom JavaMigrations i Flyway ikke har en checksum kan vi endre den selv om det allerede er kjørt. Vi kunne også slettet den, men det er jo kjekt å ha den for historikkens del.
    
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>

--- styringsinfo/src/main/kotlin/db/migration/V29__uppercase_enum_verdier.kt
+++ styringsinfo/src/main/kotlin/db/migration/V29__uppercase_enum_verdier.kt
@@ -1,73 +1,34 @@
 package db.migration
 
 import com.fasterxml.jackson.databind.node.ObjectNode
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.helse.rapids_rivers.isMissingOrNull
-import org.flywaydb.core.api.migration.BaseJavaMigration
-import org.flywaydb.core.api.migration.Context
-import java.sql.ResultSet
+import no.nav.helse.spre.styringsinfo.teamsak.behandling.Versjon
 
-internal class V29__uppercase_enum_verdier: BaseJavaMigration() {
-    private companion object {
-        private val objectMapper = jacksonObjectMapper()
+internal class V29__uppercase_enum_verdier: BehandlingshendelseJsonMigrering() {
+
+    override fun query() = "select * from behandlingshendelse where versjon='0.0.1' and er_korrigert=false;"
+
+    override fun nyVersjon() = Versjon.of("0.0.2")
+
+    override fun nyData(gammelData: ObjectNode): ObjectNode {
+        gammelData.oppdaterFeltFor("behandlingstatus")
+        gammelData.oppdaterFeltFor("behandlingsmetode")
+        gammelData.oppdaterFeltFor("behandlingskilde")
+        gammelData.oppdaterFeltFor("behandlingsresultat")
+        gammelData.oppdaterFeltFor("behandlingtype")
+        return gammelData
     }
-    override fun migrate(context: Context) {
-        val statement = context.connection.createStatement()
-        val batchStatement = context.connection.createStatement()
-        val query = """
-            select * from behandlingshendelse where versjon='0.0.1' and er_korrigert=false; 
-        """
-        val nyVersjon = "0.0.2"
-        statement.executeQuery(query).use {
-            while (it.next()) {
-                val interessanteFelter = it.toBehandlingshendelseRad()
-                interessanteFelter.oppdaterFeltFor("behandlingstatus")
-                interessanteFelter.oppdaterFeltFor("behandlingsmetode")
-                interessanteFelter.oppdaterFeltFor("behandlingskilde")
-                interessanteFelter.oppdaterFeltFor("behandlingsresultat")
-                interessanteFelter.oppdaterFeltFor("behandlingtype")
 
-                val insert = """
-                    insert into behandlingshendelse(sakId, behandlingId, funksjonellTid, versjon, data, siste, hendelseId, er_korrigert)
-                    select sakId, behandlingId, funksjonellTid, '$nyVersjon', '${interessanteFelter.data}'::jsonb, siste, hendelseId, false 
-                    from behandlingshendelse 
-                    where sekvensnummer=${interessanteFelter.sekvensnummer};
-                """
+    private companion object {
+        private fun String.MEGA() = when(this) {
+            "AvventerGodkjenning" -> "AVVENTER_GODKJENNING"
+            else -> this.uppercase()
+        }
 
-                val update = """
-                    update behandlingshendelse set siste=false, er_korrigert=true where sekvensnummer=${interessanteFelter.sekvensnummer};
-                """
-
-                batchStatement.addBatch(insert)
-                batchStatement.addBatch(update)
+        private fun ObjectNode.oppdaterFeltFor(feltnavn: String) {
+            path(feltnavn).takeUnless { it.isMissingOrNull() }?.let {
+                put(feltnavn, it.asText().MEGA())
             }
         }
-        batchStatement.executeBatch()
-
-        // TODOS:
-        //  1. Fjern unntakshåndteringen i DAO med uppercase()
-        //  2. Tenk en peu på korrigeringer av korrigeringer
-    }
-    fun ResultSet.toBehandlingshendelseRad() = InteressanteFelter(
-        sekvensnummer = getLong("sekvensnummer"),
-        data = objectMapper.readTree(getString("data")) as ObjectNode,
-        siste = getBoolean("siste")
-    )
-}
-
-private fun String.MEGA() = when(this) {
-    "AvventerGodkjenning" -> "AVVENTER_GODKJENNING"
-    else -> this.uppercase()
-}
-
-private fun InteressanteFelter.oppdaterFeltFor(feltnavn: String) {
-    data.path(feltnavn).takeUnless { it.isMissingOrNull() }?.let {
-        data.put(feltnavn, it.asText().MEGA())
     }
 }
-
-data class InteressanteFelter(
-    val sekvensnummer: Long,
-    val data: ObjectNode,
-    val siste: Boolean
-)
