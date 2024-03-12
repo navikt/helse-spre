4504e53064d4c042474f8ed4fa9f121f8dc8f3b5
Hege Haavaldsen <hege.haavaldsen@nav.no>
2024-03-12 14:38:24 +0100

    Migrerer etterslep av behandlingsresultat
    Migrering 38 kjørte mens appen leste inn behandlingsresultat og lagret de på gamlemåten
    Må migrere dette etterslepet
    
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

---
+++ styringsinfo/src/main/kotlin/db/migration/V39__fra_vedtak_iverksatt_til_vedtatt_vol2.kt
@@ -0,0 +1,22 @@
+package db.migration
+
+import com.fasterxml.jackson.databind.node.ObjectNode
+import no.nav.helse.spre.styringsinfo.teamsak.behandling.Versjon
+import org.intellij.lang.annotations.Language
+
+internal class V39__fra_vedtak_iverksatt_til_vedtatt_vol2: BehandlingshendelseJsonMigrering() {
+
+    @Language("postgresql")
+    override fun query(): String = """
+        select b.sekvensnummer, b.data, b.er_korrigert from behandlingshendelse b
+            where b.data ->> 'behandlingsresultat' = 'VEDTAK_IVERKSATT'
+            and b.er_korrigert = false;
+    """.trimIndent()
+
+    override fun nyVersjon(): Versjon? = null
+
+    override fun nyData(gammelData: ObjectNode): ObjectNode {
+        gammelData.put("behandlingsresultat", "VEDTATT")
+        return gammelData
+    }
+}
---
+++ styringsinfo/src/test/kotlin/db/migration/V39FraVedtakIverksattTilVedtattVol2Test.kt
@@ -0,0 +1,29 @@
+package db.migration
+
+import no.nav.helse.spre.styringsinfo.db.AbstractDatabaseTest.Companion.dataSource
+import no.nav.helse.spre.styringsinfo.teamsak.behandling.Versjon
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+internal class V39FraVedtakIverksattTilVedtattVol2Test: BehandlingshendelseJsonMigreringTest(
+    migrering = V39__fra_vedtak_iverksatt_til_vedtatt_vol2(),
+    dataSource = dataSource
+) {
+    @Test
+    fun `skal skrive om VEDTAK_IVERKSATT til VEDTATT`() {
+        val behandlingId = UUID.randomUUID()
+        val sakId = UUID.randomUUID()
+        val behandling = leggTilBehandlingshendelse(
+            sakId, behandlingId, true, Versjon.of("0.1.0"), false, data = {
+                it.put("behandlingsresultat", "VEDTAK_IVERKSATT")
+            }
+        )
+
+        migrer()
+        assertKorrigert(behandling) { _, ny ->
+            assertEquals(ny.path("behandlingsresultat").asText(), "VEDTATT")
+        }
+    }
+
+}
