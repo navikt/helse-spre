4cdebe2015fb8e45bd1994750308c88d35183333
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-04-11 13:48:23 +0200

    Revert "Enda en stakkar uten tags"
    This reverts commit ca31dee78072369de54aa0a214b6ab3e298aa880.

--- styringsinfo/src/main/kotlin/no/nav/helse/spre/styringsinfo/teamsak/hendelse/VedtakFattet.kt
+++ styringsinfo/src/main/kotlin/no/nav/helse/spre/styringsinfo/teamsak/hendelse/VedtakFattet.kt
@@ -56,7 +56,7 @@ internal class VedtakFattet(
                 opprettet = packet.opprettet,
                 data = packet.blob,
                 behandlingId = packet.behandlingId,
-                tags = manuelleTags[packet.hendelseId] ?: Tags(packet.tags)
+                tags = Tags(packet.tags)
             )}
         )
 
@@ -66,8 +66,5 @@ internal class VedtakFattet(
         private fun JsonMessage.demandSykepengegrunnlagfakta() = demand("sykepengegrunnlagsfakta") {
             sykepengegrunnlagsfakta -> require(!sykepengegrunnlagsfakta.isMissingOrNull())
         }
-        private val manuelleTags = mapOf(
-            UUID.fromString("8466d3ae-df24-4aaa-926f-9f44b801b270") to Tags(setOf(Tag.Forlengelse,Tag.EnArbeidsgiver, Tag.Arbeidsgiverutbetaling, Tag.Innvilget))
-        )
     }
 }
