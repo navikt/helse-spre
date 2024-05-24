6f75ab6f357964b348864ea5aa59210f2778f317
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-05-24 07:51:21 +0200

    Fjerner en Hvorfor-verdi Spleis ikke lengre sender
    Ble fjernet i https://github.com/navikt/helse-spleis/commit/c9e714caebbb1ad3e6ed89eb5c5c6a157a21fd9b

--- oppgaver/src/main/kotlin/no/nav/helse/spre/oppgaver/VedtaksperiodeVenterRiver.kt
+++ oppgaver/src/main/kotlin/no/nav/helse/spre/oppgaver/VedtaksperiodeVenterRiver.kt
@@ -16,7 +16,6 @@ class VedtaksperiodeVenterRiver(
             validate { it.demandValue("@event_name", "vedtaksperiode_venter") }
             validate { it.demandAny("venterPå.venteårsak.hva", listOf("GODKJENNING", "SØKNAD", "INNTEKTSMELDING")) }
             validate { it.requireKey("hendelser", "@id", "organisasjonsnummer", "venterPå.organisasjonsnummer") }
-            validate { it.interestedIn("venterPå.venteårsak.hvorfor") } // TODO: Fjern
         }.register(this)
     }
 
@@ -28,9 +27,7 @@ class VedtaksperiodeVenterRiver(
         if (packet["venterPå.venteårsak.hva"].asText() == "INNTEKTSMELDING") {
             val organisasjonsnummer = packet["organisasjonsnummer"].asText()
             val venterPåOrganisasjonsnummer = packet["venterPå.organisasjonsnummer"].asText()
-            val hvorfor = packet["venterPå.venteårsak.hvorfor"].asText()
-            if (hvorfor == "MANGLER_TILSTREKKELIG_INFORMASJON_TIL_UTBETALING_ANDRE_ARBEIDSGIVERE") håndter(packet) // TODO: Denne skal knertes. Når det gjøres kan nok dette løses i river validation
-            if (organisasjonsnummer != venterPåOrganisasjonsnummer) håndter(packet) // Utsetter kun om vi venter på annen AG
+            if (organisasjonsnummer != venterPåOrganisasjonsnummer) håndter(packet) // Utsetter kun om vi venter på IM annen AG
         }
         else håndter(packet) // Når vi venter på GODKJENNING/SØKNAD så håndterer vi alltid
     }
