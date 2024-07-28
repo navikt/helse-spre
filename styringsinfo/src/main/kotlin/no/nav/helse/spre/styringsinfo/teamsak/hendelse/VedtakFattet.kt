475dd1b88f56ab6368d3de9322c1aeb81bdce3a4
Øyvind Hagen <oyvind.hagen@nav.no>
2024-07-28 11:09:01 +0200

    🩹 Setter tags manuelt for melding der disse mangler

--- styringsinfo/src/main/kotlin/no/nav/helse/spre/styringsinfo/teamsak/hendelse/VedtakFattet.kt
+++ styringsinfo/src/main/kotlin/no/nav/helse/spre/styringsinfo/teamsak/hendelse/VedtakFattet.kt
@@ -67,7 +67,8 @@ internal class VedtakFattet(
             sykepengegrunnlagsfakta -> require(!sykepengegrunnlagsfakta.isMissingOrNull())
         }
         private val manuelleTags = mapOf(
-            UUID.fromString("c8a34695-24a0-4df8-9217-a95bf6eeb87a") to Tags(setOf(Tag.Forlengelse, Tag.EnArbeidsgiver, Tag.Personutbetaling, Tag.Innvilget))
+            UUID.fromString("c8a34695-24a0-4df8-9217-a95bf6eeb87a") to Tags(setOf(Tag.Forlengelse, Tag.EnArbeidsgiver, Tag.Personutbetaling, Tag.Innvilget)),
+            UUID.fromString("2b04a45a-2090-4abc-becc-b0d0150a2135") to Tags(setOf(Tag.Forlengelse, Tag.EnArbeidsgiver, Tag.Arbeidsgiverutbetaling, Tag.Innvilget))
         )
     }
 }
