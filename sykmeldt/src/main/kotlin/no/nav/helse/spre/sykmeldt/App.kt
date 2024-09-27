ae5dd8114f24f6b1ba0bc61cdea8d18adc688483
Amalie Erdal Mansåker <amalie.erdal.mansaker@nav.no>
2024-09-27 10:43:30 +0200

    Er dette nok for å lage en ny minimal app?
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>

---
+++ sykmeldt/src/main/kotlin/no/nav/helse/spre/sykmeldt/App.kt
@@ -0,0 +1,18 @@
+package no.nav.helse.spre.sykmeldt
+
+import io.ktor.server.application.*
+import io.ktor.server.cio.*
+import io.ktor.server.engine.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main() {
+
+    embeddedServer(CIO, port = 8080) {
+        routing {
+            get("/isalive") { call.respondText("ALIVE!") }
+            get("/isready") { call.respondText("READY!") }
+        }
+    }.start(wait = true)
+
+}
