cdf336eb0991988e24a557eafed0c7ce38613bf4
Amalie Erdal Mansåker <amalie.erdal.mansaker@nav.no>
2024-10-25 13:23:11 +0200

    Bumper ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 val gradlewVersion = "8.5"
 val junitJupiterVersion = "5.10.2"
 val rapidsAndRiversVersion = "2024020507581707116327.1c34df474331"
-val ktorVersion = "2.3.7" // should be set to same value as rapids and rivers
+val ktorVersion = "2.3.12" // should be set to same value as rapids and rivers
 val mockkVersion = "1.13.9"
 val testcontainersVersion = "1.19.5"
 val hikariCPVersion = "5.1.0"
