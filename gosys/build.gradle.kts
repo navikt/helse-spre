7c7146f072adde3f6455876450226c72b5a105d8
david steinsland <david@davidsteinsland.net>
2024-11-15 10:43:26 +0100

    bumper wiremock

--- gosys/build.gradle.kts
+++ gosys/build.gradle.kts
@@ -5,7 +5,7 @@ val postgresqlVersion: String by project
 val kotliqueryVersion: String by project
 val flywayCoreVersion: String by project
 val tbdLibsVersion: String by project
-
+val wiremockVersion = "3.3.1"
 val jsonassertVersion = "1.5.0"
 
 val kotlinxSerializationJsonVersion = "1.6.0"
@@ -38,5 +38,5 @@ dependencies {
     testImplementation("org.skyscreamer:jsonassert:$jsonassertVersion")
     testImplementation("io.ktor:ktor-client-mock-jvm:$ktorVersion")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
-    testImplementation("org.wiremock:wiremock:3.3.1")
+    testImplementation("org.wiremock:wiremock:$wiremockVersion")
 }
