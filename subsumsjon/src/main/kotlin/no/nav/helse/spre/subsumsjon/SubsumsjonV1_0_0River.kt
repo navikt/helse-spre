b4b1d1ed060afaed917179f60fc5233710f25752
Christian Bülow Skovborg <christianskovborg@gmail.com>
2025-02-17 20:03:20 +0100

    🚑️ Hopp over melding i dev i den andre riveren også

--- subsumsjon/src/main/kotlin/no/nav/helse/spre/subsumsjon/SubsumsjonV1_0_0River.kt
+++ subsumsjon/src/main/kotlin/no/nav/helse/spre/subsumsjon/SubsumsjonV1_0_0River.kt
@@ -25,6 +25,7 @@ internal class SubsumsjonV1_0_0River(
             precondition {
                 it.requireValue("@event_name", "subsumsjon")
                 it.requireValue("subsumsjon.versjon", "1.0.0")
+                it.forbidValues("@id", listOf("4f8595c1-3976-4bbf-9847-e653c24ad445"))
             }
             validate { it.requireKey("@id") }
             validate { it.requireKey("@opprettet") }
