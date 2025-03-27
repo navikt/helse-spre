d658799ba605e1bfbbec9d814af68df3ea75a162
david steinsland <david@davidsteinsland.net>
2025-03-27 20:15:59 +0100

    bumper avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,15 @@ plugins {
     kotlin("jvm") version "2.1.20"
 }
 
-val junitJupiterVersion = "5.11.3"
-val rapidsAndRiversVersion = "2025012712551737978926.de930d8e0feb"
-val ktorVersion = "3.0.1" // should be set to same value as rapids and rivers
-val mockkVersion = "1.13.13"
-val hikariCPVersion = "6.1.0"
+val junitJupiterVersion = "5.12.1"
+val rapidsAndRiversVersion = "2025032719521743101565.4abbf0c3af65"
+val ktorVersion = "3.1.2" // should be set to same value as rapids and rivers
+val mockkVersion = "1.13.17"
+val hikariCPVersion = "6.3.0"
 val kotliqueryVersion = "1.9.0"
-val postgresqlVersion = "42.7.4"
+val postgresqlVersion = "42.7.5"
 val flywayCoreVersion = "10.21.0"
-val tbdLibsVersion = "2025.01.27-12.50-76316f3b"
+val tbdLibsVersion = "2025.03.27-18.30-c228796d"
 
 buildscript {
     repositories { mavenCentral() }
