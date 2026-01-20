9908e5f68e07d12c7a7ae782d6125a6beed82b0c
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2026-01-20 15:09:47 +0100

    Det tøysete eventet tror jeg ikke vi trenger lengre

--- gosys/src/main/kotlin/no/nav/helse/spre/gosys/annullering/VedtaksperiodeAnnullertRiver.kt
+++ gosys/src/main/kotlin/no/nav/helse/spre/gosys/annullering/VedtaksperiodeAnnullertRiver.kt
@@ -34,7 +34,7 @@ internal class VedtaksperiodeAnnullertRiver(
     init {
         River(rapidsConnection).apply {
             precondition {
-                it.requireAny("@event_name", listOf("vedtaksperiode_annullert", "vedtaksperiode_forkastet_ikke_annullert"))
+                it.requireValue("@event_name", "vedtaksperiode_annullert")
             }
             validate { message ->
                 message.requireKey("vedtaksperiodeId", "@id")
