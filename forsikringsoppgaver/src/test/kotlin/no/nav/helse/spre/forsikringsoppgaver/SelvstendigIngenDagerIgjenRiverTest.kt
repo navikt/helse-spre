c7b22a65dcde51694a5f21066f187bbd9d8839dc
Linus Johansson <linus.123@hotmail.com>
2026-03-12 10:42:29 +0100

    Tester rett mott dev-fss
    Co-authored-by: Roy A G <43439497+royag-nav@users.noreply.github.com>

--- forsikringsoppgaver/src/test/kotlin/no/nav/helse/spre/forsikringsoppgaver/SelvstendigIngenDagerIgjenRiverTest.kt
+++ forsikringsoppgaver/src/test/kotlin/no/nav/helse/spre/forsikringsoppgaver/SelvstendigIngenDagerIgjenRiverTest.kt
@@ -21,7 +21,7 @@ class SelvstendigIngenDagerIgjenRiverTest {
 
     @Test
     fun `oppretter gosysoppgave`() {
-        // givens
+        // given
         forsikringsgrunnlagClient.forsikringsgrunnlag = Forsikringsgrunnlag(
             premiegrunnlag = "1000",
             dag1Eller17 = 1,
