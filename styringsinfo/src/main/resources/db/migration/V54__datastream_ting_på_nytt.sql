d1a82ea598994148952b9dfcddced39ef17df972
Sivert Lunsæter <sfl@computas.com>
2026-04-16 16:13:56 +0200

    Splitt siste migrering i to, siden postgres ikke lar deg lage replikering slot hvis du gjør noe som helst annet.
    Fjernet V54 fra flyway i prod basen, så den ikke vil validere feil på det.

--- styringsinfo/src/main/resources/db/migration/V54__datastream_ting_på_nytt.sql
+++ styringsinfo/src/main/resources/db/migration/V54__datastream_ting_på_nytt.sql
@@ -26,11 +26,3 @@ DO $$BEGIN
                 ON ALL TABLES IN SCHEMA public TO "bigquery-datastream";
     END IF;
 END$$;
-
--- 2. Lag replication slot om den ikke allerede finnes
-DO $$BEGIN
-    IF
-        NOT EXISTS (SELECT 1 FROM pg_replication_slots WHERE slot_name = 'spre_styringsinfo_replication') THEN
-        PERFORM pg_create_logical_replication_slot('spre_styringsinfo_replication', 'pgoutput');
-    END IF;
-END$$;
