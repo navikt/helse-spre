a71e08fad7879b6197598b6e60caefd6553fc7a5
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2026-06-04 12:48:08 +0200

    Riktig oppsett av context parameter for TransactionalSession
    ChatGPT og jeg ble enige om at det ikke er lurt å blande function type
    with receiver og context parameters.

--- gosys/src/main/kotlin/no/nav/helse/spre/gosys/vedtakFattet/SessionFactory.kt
+++ gosys/src/main/kotlin/no/nav/helse/spre/gosys/vedtakFattet/SessionFactory.kt
@@ -5,12 +5,10 @@ import kotliquery.TransactionalSession
 import kotliquery.sessionOf
 
 class SessionFactory(val dataSource: DataSource) {
-    inline fun <T> transactionally(block: TransactionalSession.() -> T): T =
+    inline fun <T> transactionally(block: context(TransactionalSession) () -> T): T =
         sessionOf(dataSource).use {
             it.transaction { transactionalSession ->
-                context(transactionalSession) {
-                    transactionalSession.block()
-                }
+                block(transactionalSession)
             }
         }
 }
